bb16b070b8230c32d49d0f4bbe5c623fc43c376e
Robin <robin@macbookair.home>
2023-03-06 20:22:57 +0100

    Inserted the new Chat and ChatInput component

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,7 +1,19 @@
-function ChatPage() {
+import Chat from "../../../components/Chat"
+import ChatInput from "../../../components/ChatInput"
+
+type Props = {
+  params: {
+    id: string;
+  }
+}
+
+function ChatPage({ params: { id } }: Props) {
   return (
-    <div>
-      ChatPage
+    <div className='flex flex-col h-screen overflow-hidden'>
+      {/* Chat */}
+      <Chat chatId={id} />
+      {/* ChatInput */}
+      <ChatInput chatId={id} />
     </div>
   )
 }
