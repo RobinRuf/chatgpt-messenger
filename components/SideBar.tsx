2a86c29334d8b30cce3607b68a73c78c6071968d
Robin <robin@macbookair.home>
2023-03-06 01:16:40 +0100

    Changed the get data to a query and added orderBy

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -2,9 +2,16 @@
 
 import NewChat from '../components/NewChat'
 import { useSession, signOut } from 'next-auth/react'
+import { collection, query, orderBy } from 'firebase/firestore'
+import { useCollection } from 'react-firebase-hooks/firestore'
+import { db } from '../firebase';
+import ChatRow from '../components/ChatRow'
 
 function SideBar() {
   const { data: session } = useSession();
+  const [chats, loading, error] = useCollection(
+    session && query(collection(db, 'users', session.user?.email!, 'chats'), orderBy('createdAt', 'asc'))
+  );
 
   return <div className='p-2 flex flex-col h-screen'>
     <div className='flex-1'>
@@ -12,10 +19,14 @@ function SideBar() {
         {/* NewChat */}
         <NewChat />
 
-        <div> {/* ModelSelection */}
+        <div> 
+          {/* ModelSelection */}
         </div>
 
           { /* Map through the Chat-Rows */}
+        {chats?.docs.map(chat => (
+        <ChatRow key={chat.id} id={chat.id} />
+        ))}
       </div> 
     </div>
 
