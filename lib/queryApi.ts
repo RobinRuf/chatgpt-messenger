73e9287965fbd397e0034701c9ad8b8b3306ee65
Robin <robin@macbookair.home>
2023-03-08 01:37:10 +0100

    Created Helper Functions for API calls and backend communication

---
+++ lib/queryApi.ts
@@ -0,0 +1,17 @@
+import openai from './chatgpt'
+
+const query = async (prompt: string, chatId: string, model: string) => {
+  const res = await openai.createCompletion({
+    model,
+    prompt,
+    temperature: 0.9, // temperature and top_p defines if the answer should be more logical or more creative
+    top_p: 1,
+    max_tokens: 1000,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  }).then(res => res.data.choises(0).text).catch(err => `ChatGPT was not able to find an answer for that! Error: ${err.message}`)
+
+  return res;
+}
+
+export default query
