fb0fc095e35d790aa9d04035e7a1fde964a3e1a0
Robin <robin@macbookair.home>
2023-03-14 18:35:33 +0100

    Created new path settings

---
+++ app/settings/page.tsx
@@ -0,0 +1,10 @@
+function SettingsPage() {
+  return (
+    <div className='flex flex-col items-center justify-center text-white h-screen px-2'>
+      <h1 className='text-5xl font-bold mb-20'>Settings</h1>
+
+    </div>
+  )
+}
+
+export default SettingsPage
